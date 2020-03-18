f882bde84ba5dcdfac1ab27f55bb70011de744ec
Perry Wang <perryw@nvidia.com>
2020-03-18 02:06:52 -0700

    test console

--- filtering.js
+++ filtering.js
@@ -7,6 +7,8 @@
   $(document).ready(function () {
     alert(1);
     alert(2);
+    console.log(">>>>>>>>>1111");
+    console.log(">>>>>>>>>2222");
     tableau.extensions.initializeAsync().then(function () {
       fetchFilters();
 
