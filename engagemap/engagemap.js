20d8b8e65365251285535c29d8ad8a70555e87e6
Perry Wang <perryw055@gmail.com>
2020-05-11 07:04:57 -0700

    test time

--- engagemap/engagemap.js
+++ engagemap/engagemap.js
@@ -2,27 +2,14 @@
 
 (function () {
   $(document).ready(function () {
-    const table = $('#parameterTable');
-    const tableBody = table.children('tbody');
-
     // This is the entry point into the extension.  It initializes the Tableau Extensions Api, and then
     // grabs all of the parameters in the workbook, processing each one individually.
     tableau.extensions.initializeAsync().then(function () {
       tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(function (parameters) {
         parameters.forEach(function (p) {
           p.addEventListener(tableau.TableauEventType.ParameterChanged, onParameterChange);
-          parameterRow(p).appendTo(tableBody);
+          parameterRow(p);
         });
-
-        // This is used to manipulate what part of the UI is visible.  If there are no parameters
-        // found, we want to give you a message to tell you that you need to add one, otherwise, we
-        // show the table we created.
-        $('#loading').addClass('hidden');
-        if (parameters.length === 0) {
-          $('#addParameterWarning').removeClass('hidden').addClass('show');
-        } else {
-          $('#parameterTable').removeClass('hidden').addClass('show');
-        }
       });
     });
   });
